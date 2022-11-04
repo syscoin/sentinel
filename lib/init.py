e40100564e1f2cf69b3516d1a9ad11c47959aed3
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2022-11-04 10:39:52 -0700

    fix docker

--- lib/init.py
+++ lib/init.py
@@ -94,9 +94,9 @@ def main():
         print("Please ensure correct database configuration.")
         sys.exit(1)
 
-    if not has_syscoin_conf():
-        print("Syscoin must be installed and configured, including JSONRPC access in syscoin.conf")
-        sys.exit(1)
+    #if not has_syscoin_conf():
+        #print("Syscoin must be installed and configured, including JSONRPC access in syscoin.conf")
+        #sys.exit(1)
 
 
 main()
